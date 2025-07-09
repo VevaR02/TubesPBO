c12501abf5329d73ec69f89c8aa853a0d965fb9f
VevaR02 <101241106+VevaR02@users.noreply.github.com>
2025-07-09 12:26:43 +0700

    Update ProductRepository.java

--- src/main/java/com/sevspo/marketplace/repository/ProductRepository.java
+++ src/main/java/com/sevspo/marketplace/repository/ProductRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.sevspo.marketplace.model.Product;
 
-// PERBAIKAN: Menambahkan JpaSpecificationExecutor untuk query dinamis
+
 public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
 }
